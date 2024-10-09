c4230895f434e7b01140757e1010bc94ef55b942
xisurthros <jacobcoburnhobbs@gmail.com>
2024-10-08 22:49:33 -0500

    working twoSum - cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,28 +8,46 @@ class LeetCodeProblems {
 		}
 
 		std::vector<int> twoSum(std::vector<int>& nums, int target) {
-			int x = 0;
-			int y = 1;
-			int size_of_nums = sizeof(nums)/sizeof(nums[0]);
+			auto size_of_nums = nums.size();
 			std::cout << "size_of_nums: " << size_of_nums << std::endl;
+
+			for (int x = 0; x <= size_of_nums; x++) {
+				int required_number = target - nums[x];
+				std::cout << "Target: " << target << " Current Number:  " << nums[x] << " Required Number: " << required_number << std::endl;
+				if (nums[x + 1] == required_number) {
+					std::cout << nums[x+1] << std::endl;
+					std::cout << "FOUND!" << std::endl;
+					break;
+				}
+			}
 
 			printBreakLine();
 			return nums;
 		}
 };
 
+class LeetCodeSolutions : public LeetCodeProblems {
+	public:
+		void run_twoSum() {
+			
+			std::vector<int> nums_1 = {2, 7, 11, 15};
+			int target_1 = 9;
+
+			std::vector<int> nums_2 = {3, 2, 4};
+			int target_2 = 6;
+
+			std::vector<int> nums_3 = {3, 3};
+			int target_3 = 6;
+			
+			twoSum(nums_1, target_1);
+			twoSum(nums_2, target_2);
+			twoSum(nums_3, target_3);
+		}
+};
+
 int main(){
-	std::vector<int> nums_1 = {2, 7, 11, 15};
-	int target_1 = 9;
 
-	std::vector<int> nums_2 = {3, 2, 4};
-	int target_2 = 6;
+	LeetCodeSolutions solutions;
+	solutions.run_twoSum();
 
-	std::vector<int> nums_3 = {3, 3};
-	int target_3 = 6;
-
-	LeetCodeProblems problems;
-	problems.twoSum(nums_1, target_1);
-	problems.twoSum(nums_2, target_2);
-	problems.twoSum(nums_3, target_3);
 }
