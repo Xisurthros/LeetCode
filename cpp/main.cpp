7a8c75e557f81448531e717b7c9b10e30dca82f8
xisurthros <jacobcoburnhobbs@gmail.com>
2024-10-09 22:12:50 -0500

    unindented public

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,41 +3,41 @@
 #include <unordered_map>
 
 class LeetCodeProblems {
-	public:
-		void printBreakLine() {
-			std::cout << "=========================================================================" << std::endl;
-		}
+public:
+	void printBreakLine() {
+		std::cout << "=========================================================================" << std::endl;
+	}
 
-		std::vector<int> twoSum(std::vector<int>& nums, int target) {
-			/**
-			* Returns indices of two numbers that add up to the target
-			*
-			* @param nums - Vector of integers
-			* @param target - Target sum
-			* @return Vector with two indices, or empty if no pair is found
-			*/
+	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		/**
+		* Returns indices of two numbers that add up to the target
+		*
+		* @param nums - Vector of integers
+		* @param target - Target sum
+		* @return Vector with two indices, or empty if no pair is found
+		*/
 
-			// Map to store seen numbers and their indices
-			std::unordered_map<int, int> passed_nums = {};
-			auto size_of_nums = nums.size();
+		// Map to store seen numbers and their indices
+		std::unordered_map<int, int> passed_nums = {};
+		auto size_of_nums = nums.size();
 
-			// Iterate through the vector to find the pair
-			for (int x = 0; x <= size_of_nums; x++) {
-				int needed_num = target - nums[x];
+		// Iterate through the vector to find the pair
+		for (int x = 0; x <= size_of_nums; x++) {
+			int needed_num = target - nums[x];
 
-				// If the needed number exists in the map, return the pair of indices
-				if (passed_nums.find(needed_num) != passed_nums.end()) {
-					std::cout << '{' << passed_nums[needed_num] << ',' << x << '}' << std::endl;
-					return {passed_nums[needed_num], x};
-				}
-
-				// If the needed number exists in the map, return the pair of indices
-				passed_nums[nums[x]] = x;
+			// If the needed number exists in the map, return the pair of indices
+			if (passed_nums.find(needed_num) != passed_nums.end()) {
+				std::cout << '{' << passed_nums[needed_num] << ',' << x << '}' << std::endl;
+				return {passed_nums[needed_num], x};
 			}
 
-			// Return empty if no pair found
-			return {};
+			// If the needed number exists in the map, return the pair of indices
+			passed_nums[nums[x]] = x;
 		}
+
+		// Return empty if no pair found
+		return {};
+	}
 };
 
 class LeetCodeSolutions : public LeetCodeProblems {
